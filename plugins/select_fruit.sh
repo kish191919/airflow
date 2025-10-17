60bc5dab0aaa4103d7179210058d23a41e006c10
Danny <kish1919@gmail.com>
2025-10-16 22:09:50 -0400

    add shell script

---
+++ plugins/select_fruit.sh
@@ -0,0 +1,10 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ];then
+echo "The fruit is apple"
+elif [ $FRUIT == ORANGE ];then
+echo "The fruit is orange"
+elif [ $FRUIT == GRAPE ];then
+echo "The fruit is grape"
+else
+echo "The fruit is unknown"
+fi
